862ad674b57180eec3c7bbe56b48ad04b7643388
Christoph Bünte <info@christophbuente.de>
2013-12-06 15:43:59 +0100

    Remove obsolte attribute.

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Address.class_eval do
 
-  attr_accessible :use_packstation, :station_number, :locker_number
+  attr_accessible :station_number, :locker_number
 
   validates :station_number, length: { :minimum => 3 }, allow_blank: true
   validates :locker_number, length:  { :minimum => 7 }, allow_blank: true
